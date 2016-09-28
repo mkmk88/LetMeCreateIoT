9e6292fff1daad26775eecc506a730d5cfc0ee90
Michal Tusnio <m.tusnio@gmail.com>
2016-09-28 15:45:16 +0100

    PWM: Stubs added

---
+++ src/core/pwm.c
@@ -0,0 +1,42 @@
+#include "letmecreate/core/pwm.h"
+
+int pwm_init(uint8_t mikrobus_index)
+{
+}
+
+int pwm_enable(uint8_t mikrobus_index)
+{
+}
+
+int pwm_set_duty_cycle(uint8_t mikrobus_index, float percentage)
+{
+}
+
+int pwm_get_duty_cycle(uint8_t mikrobus_index, float *percentage)
+{
+}
+
+int pwm_set_frequency(uint8_t mikrobus_index, uint32_t frequency)
+{
+}
+
+int pwm_get_frequency(uint8_t mikrobus_index, uint32_t *frequency)
+{
+}
+
+int pwm_set_period(uint8_t mikrobus_index, uint32_t period)
+{
+}
+
+int pwm_get_period(uint8_t mikrobus_index, uint32_t *period)
+{
+}
+
+int pwm_disable(uint8_t mikrobus_index)
+{
+}
+
+int pwm_release(uint8_t mikrobus_index)
+{
+}
+
